2c92d7edaf8a37bf544fb9ec9e646fc49123a30b
Ron Kokotov <2ron.kokotov@gmail.com>
2022-05-03 14:48:12 +0300

    added basic requests and responses, switched to json encoding

--- client.py
+++ client.py
@@ -1,30 +1,67 @@
 import socket_util as su
+import json
+from os import system
+
+
+def clear():
+    system("cls")
 
 
 sock = su.client("192.168.68.137", 12345)
-running = True
-while running:
-    print("\nEnter action:\n 0 for adding to db\n 1 for retrieving from db\n e to exit")
-    action = input()
-    send_req = True
+vals_to_req = 10
+state = "start"
+while True:
+    print("\nEnter action")
+    if state == "start":
+        print("b: go back (exit)\na: add forum\n"
+              "s[]: search forum\n\tsl: by latest\n\tsn: by name\n\tsk: by keywords")
+        action = input()
+        if action == "b":
+            sock.close()
+            break
+        if action == "sl":
+            forums = list()
+            vals_requested = 0
+            last_srch_req_id = 0
+            state = "search"
+            forums = su.send_request(sock, 0, (vals_requested, vals_to_req))
+            vals_requested += len(forums)
+            print("Forums: ")
+            for id, name, desc in forums:
+                print(f"id: {id}| Name: {name}, description: {desc}")
+        elif action == "a":
+            print("Enter forum name:")
+            name = input()
+            print("Enter forum description:")
+            desc = input()
+            print(su.send_request(sock, 1, (name, desc)))
+        else:
+            print("Invalid action")
+            send_req = False
 
-    if action == "0":
-        print("Enter text to add to db")
-        send_data = input().encode()
-    elif action == "1":
-        print("Enter start index of send_data points")
-        s = input()
-        print("Enter end index of send_data points")
-        e = input()
-        send_data = f"{s},{e}".encode()
+    elif state == "search":
+        print("b: go back (start)\nm: show more forums\ne: enter forum")
+        action = input()
+        if action == "b":
+            state = "start"
+            continue
+        if action == "m":
+            forums = su.send_request(sock, last_srch_req_id, (vals_requested, vals_to_req))
+            len_forums = len(forums)
+            if len_forums == 0:
+                print("No more forums")
+                continue
+            for i, x in enumerate(forums):
+                print(f"{i + vals_requested}: {x}")
+            vals_requested += len_forums
+        elif action == "e":
+            print("Enter forum id")
+            f_i = input()
+            to_enter = forums[int(f_i)]
+            state = "forum"
+        else:
+            print("Invalid action")
+            send_req = False
 
-    elif action == "e":
-        sock.close()
-        running = False
-        send_req = False
-    else:
-        print("invalid action")
-        send_req = False
-    if send_req:
-        print(su.send_request(sock, int(action), send_data).decode())
-
+    elif state == "forum":
+        pass
--- server.py
+++ server.py
@@ -1,40 +1,51 @@
 import socket_util as su
+import json
 from functools import reduce
 
 
+forum_id = 0  # global
+
+
 def reply(req_id, data):
+    data = data.decode()
     if req_id == 0:
-        dbase.append(str.lower(data.decode()))
-        dbase.sort()
-        return su.add_len_bytes("ack".encode())
-    elif req_id == 1:
-        start, end = data.decode().split(",")
-        send_data = reduce(lambda x, y: f"{x}, {y}", dbase[int(start):int(end)]).encode()
-        return su.add_len_bytes(send_data)
+        start, end = json.loads(data)
+        to_send = list()
+        for forum in dbase[start:end]:
+            to_send.append((forum["id"], forum["name"], forum["description"]))
+        return json.dumps(to_send).encode()
+    if req_id == 1:
+        name, desc = json.loads(data)
+        forum = forum_template.copy()
+        forum["name"] = name
+        forum["description"] = desc
+        global forum_id
+        forum["id"] = forum_id
+        forum_id += 1
+        dbase.append(forum)
+        return json.dumps("Added to database").encode()
 
 
-forum = {"id": None,
+forum_template = {"id": None,
          "name": None,
          "description": None,
          "kwrds": None,
          "users": None,
          "posts": None}
 
-post = {"user": None,
+post_template = {"user": None,
         "title": None,
         "text": None,
         "img_path": None,
         "comments": None}
 
-comment = {"user": None,
+comment_template = {"user": None,
            "text": None,
            "img_path": None}
 
-user = {"name": None,
+user_template = {"name": None,
         "hashed_pass": None,
         "forums": None}
 
 dbase = list()
-dbase.sort()
-
 su.server(12345, reply)
